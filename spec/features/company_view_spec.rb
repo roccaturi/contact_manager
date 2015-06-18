0ff92f7365a96844b1656ff838a020f20ddf1f1f
roccaturi <brian.burnley@gmail.com>
2015-06-18 14:25:02 -0400

    Correct for 'git commit -am MESSAGE not adding newly created files

---
+++ spec/features/company_view_spec.rb
@@ -0,0 +1,107 @@
+require 'rails_helper'
+
+describe 'the company view', type: :feature do
+  let(:company) {Company.create(name: 'Rossum\'s Universal Robots' )}
+
+  describe 'for phone numbers' do
+    before(:each) do
+      company.phone_numbers.create(number: '555-1234')
+      company.phone_numbers.create(number: '555-9876')
+      visit company_path(company)
+    end
+
+    it 'shows the phone numbers' do
+      company.phone_numbers.each do |phone|
+        expect(page).to have_content(phone.number)
+      end
+    end
+    it 'has a link to add a new phone number' do
+      expect(page).to have_link('Add phone number', href: new_phone_number_path(contact_id: company.id, contact_type: 'Company'))
+    end
+    it 'adds a new phone number' do
+      page.click_link('Add phone number')
+      page.fill_in('Number', with: '555-8888')
+      page.click_button('Create Phone number')
+      expect(current_path).to eq(company_path(company))
+      expect(page).to have_content('555-8888')
+    end
+    it 'has links to edit phone numbers' do
+      company.phone_numbers.each do |phone|
+        expect(page).to have_link('edit', href: edit_phone_number_path(phone))
+      end
+    end
+    it 'edits a phone number' do
+      phone = company.phone_numbers.first
+      old_number = phone.number
+      first(:link, 'edit').click
+      page.fill_in('Number', with: '555-5432')
+      page.click_button('Update Phone number')
+      expect(current_path).to eq(company_path(company))
+      expect(page).to have_content('555-5432')
+      expect(page).to_not have_content(old_number)
+    end
+    it 'has links to delete phone numbers' do
+      company.phone_numbers.each do |phone|
+        expect(page).to have_link('delete', href: phone_number_path(phone))
+      end
+    end
+    it 'destroys a deleted phone number' do
+      phone = company.phone_numbers.first
+      target_number = phone.number
+      first(:link, 'delete').click
+      expect(current_path).to eq(company_path(company))
+      expect(page).to_not have_content(target_number)
+    end
+  end
+
+  describe 'for email addresses' do
+    before(:each) do
+      company.email_addresses.create(address: 'hello@someguy.com')
+      company.email_addresses.create(address: 'me@someguy.com')
+      visit company_path(company)
+    end
+
+    it 'shows the email addresses' do
+      company.email_addresses.each do |email|
+        expect(page).to have_selector('li', text: email.address.to_s + " edit delete")
+      end
+    end
+    it 'has a link to add a new email address' do
+      expect(page).to have_link('Add email address', href: new_email_address_path(contact_id: company.id, contact_type: 'Company'))
+    end
+    it 'adds a new email address' do
+      page.click_link('Add email address')
+      page.fill_in('Address', with: 'test@testly.io')
+      page.click_button('Create Email address')
+      expect(current_path).to eq(company_path(company))
+      expect(page).to have_content('test@testly.io')
+    end
+    it 'has links to edit email addresses' do
+      company.email_addresses.each do |email|
+        expect(page).to have_link('edit', href: edit_email_address_path(email))
+      end
+    end
+    it 'edits an email address' do
+      email = company.email_addresses.first
+      old_address = email.address
+      first(:link, 'edit').click
+      page.fill_in('Address', with: 'new@address.com')
+      page.click_button('Update Email address')
+      expect(current_path).to eq(company_path(company))
+      expect(page).to have_content('new@address.com')
+      expect(page).to_not have_content(old_address)
+    end
+    it 'has links to delete email addresses' do
+      company.email_addresses.each do |email|
+        expect(page).to have_link('delete', href: email_address_path(email))
+      end
+    end
+    it 'destroys a deleted email address' do
+      email = company.email_addresses.first
+      target_address = email.address
+      first(:link, 'delete').click
+      expect(current_path).to eq(company_path(company))
+      expect(page).to_not have_content(target_address)
+    end
+  end
+end
